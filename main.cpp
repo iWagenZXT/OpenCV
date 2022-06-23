e03261834cb8f3d4f80c2904c9dc1048648775da
WagenXiaoteng <1845656822@qq.com>
2022-06-23 14:33:29 +0800

    test

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 #include<opencv2/core/core.hpp>
 #include<opencv2/highgui/highgui.hpp>
 #include<opencv2/opencv.hpp>
-//#include<opencv2/imgproc.hpp>
-//#include<opencv2/imgproc/imgproc.hpp>
-
+#include<opencv2/imgproc.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
 
 int main() {
 
